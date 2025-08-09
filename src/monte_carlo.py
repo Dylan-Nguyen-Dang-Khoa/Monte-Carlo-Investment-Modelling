a757b8af6e0714b6e3dcb657cbcf8c7f4d31583b
Dylan_Nguyen_Dang_Khoa <Dylan_Nguyen_Dang_Khoa@s2023.ssts.edu.sg>
2025-08-09 17:35:22 +0800

    Added some small improvements, like calculation of metrics

--- src/monte_carlo.py
+++ src/monte_carlo.py
@@ -6,19 +6,17 @@ import streamlit as st
 
 
 class Stock:
-    def __init__(self, ticker="GC=F", period="3y"):
+    def __init__(self, ticker="GC=F", period="3y", interval="1d"):
         self.data = yf.download(tickers=ticker, period=period, auto_adjust=True)
-        self.average_log_returns_and_volatility()
+        self.calculate_metrics()
 
-    def average_log_returns_and_volatility(self) -> None:
-        log_returns = []
-        previous_close = None
-        for index, row in self.data.iterrows():
-            if previous_close is not None:
-                log_returns.append(np.log(row["Close"] / previous_close))
-            previous_close = row["Close"]
-        self.average_log_returns = np.mean(log_returns)
-        self.sigma = np.std(log_returns, ddof=1)
+    def calculate_metrics(self) -> None:
+        self.data["LogReturn"] = np.log(
+            self.data["Close"] / self.data["Close"].shift(1)
+        )
+        log_returns = self.data["LogReturn"].dropna()
+        self.average_log_returns = log_returns.mean()
+        self.sigma = log_returns.std(ddof=1)
 
 
 class MonteCarlo:
@@ -26,21 +24,18 @@ class MonteCarlo:
         self.N = N
         self.dT = T / N
         self.S0 = last_price
-        self.drift = average_log_returns - 0.5 * sigma**2
-        self.scaled_sigma = sigma * N**0.5
+        self.drift = average_log_returns
+        self.sigma = sigma
 
     def geometric_brownian_motion(self, num_simulations=100):
-        self.W_t = np.random.normal(size=(num_simulations, self.N))
-        prices = np.full(shape=(num_simulations, 1), fill_value=self.S0)
+        self.simulated_prices = np.full(shape=(num_simulations, 1), fill_value=self.S0)
         for step in range(self.N):
-            step_prices = prices[:, step] * np.e ** (
-                (self.drift - self.scaled_sigma**2 / 2) * self.dT
-                + self.scaled_sigma
-                * self.dT**0.5
-                * np.random.normal(size=(num_simulations,))
+            step_prices = self.simulated_prices[:, step] * np.exp(
+                (self.drift - self.sigma**2 / 2) * self.dT
+                + self.sigma * self.dT**0.5 * np.random.normal(size=(num_simulations,))
             )
             step_prices = step_prices.reshape(-1, 1)
-            prices = np.hstack((prices, step_prices))
+            self.simulated_prices = np.hstack((self.simulated_prices, step_prices))
 
 
 gold = Stock()
@@ -50,3 +45,4 @@ simulation = MonteCarlo(
     sigma=gold.sigma,
 )
 simulation.geometric_brownian_motion()
+print(simulation.simulated_prices[:5, -1])
